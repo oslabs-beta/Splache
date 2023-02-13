ba3c4b561fe3c59d2661e823448ef4ac5a9f1a1c
Nicholas Cathcart <nicholascathcart@Nicholass-MacBook-Air.local>
2023-02-13 18:59:33 -0500

    made some progress on SlacheCache

--- src/index-test.ts
+++ src/index-test.ts
@@ -2,20 +2,21 @@ import express from 'express';
 import { SplacheCache } from './SplacheCache';
 import {SplacheCacheWhole} from './CacheWhole'
 import {StarWarsSchema} from './schemaTest'
+// import {StarWarsSchema} from './SchemaWithCache'
 import {graphql, GraphQLSchema} from 'graphql'
 
 const app = express()
 
-const cache = new SplacheCacheWhole(StarWarsSchema);
-// const cache = new SplacheCache(StarWarsSchema)
+// const cache = new SplacheCacheWhole(StarWarsSchema);
+const cache = new SplacheCache(StarWarsSchema)
 app.use(express.json())
-app.use('/graphql', cache.wholeCache, (req, res) => {
-  res.send(res.locals.queryResult)
-})
-
-// app.use('/graphql', cache.GQLquery, (req, res) => {
+// app.use('/graphql', cache.wholeCache, (req, res) => {
 //   res.send(res.locals.queryResult)
 // })
+
+app.use('/graphql', cache.GQLquery, (req, res) => {
+  res.send(res.locals.queryResult)
+})
 // app.use('/graphql', (req, res) => {
 //   graphql({ schema: StarWarsSchema, source: req.body.query})
 //     .then((response) => {
