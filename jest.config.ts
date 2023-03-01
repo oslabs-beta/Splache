e398c3fed557cd80998294678b828760862a6e39
NicJax <nicolasljax@gmail.com>
2023-03-01 14:25:29 -0500

    deleted linter,

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@jest/types"
-
-const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  verbose: true,
-  automock: false,
-}
-export default config;
