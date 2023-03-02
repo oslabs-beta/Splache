b71efe8c0b23586a0f86309febf133f957db6fde
Nicholas Cathcart <nicholascathcart@Nicholass-MacBook-Air.local>
2023-03-02 09:11:53 -0500

    update for test package

--- src/index-test.ts
+++ src/index-test.ts
@@ -1,16 +1,16 @@
 import express from 'express';
-import { SplacheCache } from './SplacheCache';
-import {SplacheCacheWhole} from './CacheWhole'
+import { SplacheCache } from 'test-test-splache';
+import {SplacheCacheWhole} from 'test-test-splache'
 import {StarWarsSchema} from './schemaTest'
 // import {StarWarsSchema} from './SchemaWithCache'
 import {graphql, GraphQLSchema} from 'graphql'
 
 const app = express()
 
-const cache = new SplacheCacheWhole(StarWarsSchema);
+const cache = new SplacheCache(StarWarsSchema);
 // const cache = new SplacheCache(StarWarsSchema)
 app.use(express.json())
-app.use('/graphql', cache.wholeCache, (req, res) => {
+app.use('/graphql', cache.GQLquery, (req, res) => {
   res.send(res.locals.queryResult)
 })
 
